b0238da5684b49456638d0007c759b7061c5da4f
Ana Luisa Dias <analudias@live.com>
2021-01-14 21:32:31 -0300

    Initial commit

---
+++ src/main/java/com/purplepizza/clientcrud/ClientcrudApplication.java
@@ -0,0 +1,13 @@
+package com.purplepizza.clientcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientcrudApplication.class, args);
+	}
+
+}
