6fbe1c98269c5fa96c3fe13a372157b5751d11ce
Ana Luisa Dias <analudias@live.com>
2021-01-20 20:23:07 -0300

    Update and insert

--- src/main/java/com/purplepizza/clientcrud/resources/ClientResource.java
+++ src/main/java/com/purplepizza/clientcrud/resources/ClientResource.java
@@ -1,11 +1,17 @@
 package com.purplepizza.clientcrud.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.purplepizza.clientcrud.dto.ClientDTO;
 import com.purplepizza.clientcrud.services.ClientService;
@@ -21,6 +27,20 @@ public class ClientResource {
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
 		ClientDTO dto = clientService.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+		dto = clientService.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = clientService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
--- src/main/java/com/purplepizza/clientcrud/services/ClientService.java
+++ src/main/java/com/purplepizza/clientcrud/services/ClientService.java
@@ -2,6 +2,8 @@ package com.purplepizza.clientcrud.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +23,34 @@ public class ClientService {
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = clientRepository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = clientRepository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setEmail(dto.getEmail());
+			entity.setAddress(dto.getAddress());
+			entity.setPhoneNumber(dto.getPhoneNumber());
+			entity.setCpf(dto.getCpf());
+			entity.setSex(dto.getSex());
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " +id);
+		}
+	}
+	
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setAddress(dto.getAddress());
+		entity.setPhoneNumber(dto.getPhoneNumber());
+		entity.setCpf(dto.getCpf());
+		entity.setSex(dto.getSex());
+		entity = clientRepository.save(entity);
 		return new ClientDTO(entity);
 	}
 
