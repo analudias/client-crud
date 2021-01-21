0b7e5dd4f5fa37f3624194033700fa92f4cbf592
Ana Luisa Dias <analudias@live.com>
2021-01-20 21:23:05 -0300

    Exception handler

--- src/main/java/com/purplepizza/clientcrud/resources/exceptions/StandardError.java
+++ src/main/java/com/purplepizza/clientcrud/resources/exceptions/StandardError.java
@@ -1,5 +1,64 @@
 package com.purplepizza.clientcrud.resources.exceptions;
 
-public class StandardError {
+import java.io.Serializable;
+import java.sql.Date;
+import java.time.Instant;
 
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Instant timeStamp;
+	private int status;
+	private String error;
+	private String msg;
+	private String path;
+	
+	public StandardError() {
+	}
+
+	public Instant getTimestamp() {
+		return timeStamp;
+	}
+
+	public void setTimestamp(Instant timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 }
